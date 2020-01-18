09792308f7d9a428f7d609e55b6807849b22d2de
Danilo Pinotti <danilopinotti@hotmail.com>
2020-01-18 16:18:47 -0300

    Initial Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mt-5">
+      <div class="text-center">
+          <img alt="Vue logo" height="100px" src="../assets/logo.png">
+      </div>
+
+      <div class="text-center mt-3">
+          <span class="text-primary h3">Talk Sobre Vue 2</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+</style>
