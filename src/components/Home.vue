f835a49e5648ae0c6082c3509bb07a2fdc0ad4a4
Danilo Pinotti <danilopinotti@hotmail.com>
2020-01-18 18:23:26 -0300

    Implementar exemplos simples de binding

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="mt-5">
+  <div class="pt-5" :class="{'bg-dark': usingDarkTheme}">
       <div class="text-center">
-          <img alt="Vue logo" height="100px" src="../assets/logo.png">
+          <img alt="Lets logo" height="100px" src="../assets/logo.png">
       </div>
 
       <div class="text-center mt-5">
-          <span class="alert alert-secondary">
+          <span class="alert alert-secondary cursor-default" :title="title">
               <i class="far fa-comments mr-1"></i>
-              Apresentação sobre Vue 2
+              {{ message }}
           </span>
+      </div>
+
+      <div class="text-center mt-5">
+          <button class="btn btn-primary" :disabled="!canChangeTheme">
+              <i class="far mr-1" :class="{'fa-moon': usingDarkTheme, 'fa-sun': !usingDarkTheme}"></i>
+              Trocar tema
+          </button>
       </div>
   </div>
 </template>
@@ -16,8 +23,21 @@
 <script>
   export default {
     name: 'Home',
+
+    data() {
+        return {
+            message: 'Apresentação sobre Vue 2',
+            title: 'Exemplo de título',
+
+            usingDarkTheme: false,
+            canChangeTheme: false,
+        }
+    }
   }
 </script>
 
 <style scoped>
+    .cursor-default {
+        cursor: default;
+    }
 </style>
