3c562d51eb3233e98ef66f8768e0f171753a6f0f
Danilo Pinotti <danilopinotti@hotmail.com>
2020-01-18 17:25:17 -0300

    Instalar fontawesome

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,19 @@
           <img alt="Vue logo" height="100px" src="../assets/logo.png">
       </div>
 
-      <div class="text-center mt-3">
-          <span class="text-primary h3">Talk Sobre Vue 2</span>
+      <div class="text-center mt-5">
+          <span class="alert alert-secondary">
+              <i class="far fa-comments mr-1"></i>
+              Apresentação sobre Vue 2
+          </span>
       </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-}
+  export default {
+    name: 'Home',
+  }
 </script>
 
 <style scoped>
