be0b3db1fab78216dcac57821b75ba717555e878
Danilo Pinotti <danilopinotti@hotmail.com>
2020-01-23 21:55:08 -0300

    Criar formulário para login

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,39 +1,65 @@
 <template>
-  <div class="pt-5" :class="{'bg-dark': usingDarkTheme}">
-      <div class="text-center">
-          <img alt="Lets logo" height="100px" src="../assets/logo.png">
-      </div>
+    <div class="pt-5">
+        <div class="text-center">
+            <img alt="Lets logo" height="100px" src="../assets/logo.png">
+        </div>
 
-      <div class="text-center mt-5">
-          <span class="alert alert-secondary cursor-default" :title="title">
+        <!-- Simple text binded -->
+        <div class="text-center mt-5 col-12">
+          <span class="cursor-default" :title="alertTitle">
               <i class="far fa-comments mr-1"></i>
-              {{ message }}
+              {{ alertMessage }}
           </span>
-      </div>
+        </div>
 
-      <div class="text-center mt-5">
-          <button class="btn btn-primary" :disabled="!canChangeTheme">
-              <i class="far mr-1" :class="{'fa-moon': usingDarkTheme, 'fa-sun': !usingDarkTheme}"></i>
-              Trocar tema
-          </button>
-      </div>
-  </div>
+        <div class="mt-4">
+            <!-- Form binded -->
+            <form class="col-4 offset-4">
+                <div class="form-group">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Digite seu email" v-model="loginForm.email">
+                    <small id="emailHelp" class="form-text text-muted">Não iremos compartilhar seu email com ninguém</small>
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Senha</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua senha" v-model="loginForm.password">
+                </div>
+
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="keepConnected" v-model="loginForm.keepConnected">
+                    <label class="form-check-label" for="keepConnected">Mantenha-me conectado</label>
+                </div>
+
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary mt-2" :disabled="!loginForm.canSignIn">
+                        Acessar
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
+    export default {
+        name: 'Home',
 
-    data() {
-        return {
-            message: 'Apresentação sobre Vue 2',
-            title: 'Exemplo de título',
+        data() {
+            return {
+                alertMessage: 'Apresentação sobre Vue 2',
+                alertTitle: 'Exemplo de título',
 
-            usingDarkTheme: false,
-            canChangeTheme: false,
+                loginForm: {
+                    email: '',
+                    password: '',
+                    keepConnected: false,
+                    canSignIn: false,
+                },
+
+            }
         }
     }
-  }
 </script>
 
 <style scoped>
